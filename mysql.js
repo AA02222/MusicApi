d5af6f19f441f8da72fa6ae6b9b2b4544fb73d2b
Your Name <you@example.com>
2022-07-05 00:01:18 +0800

    初始化了路由、创建了Mysql连接池

---
+++ mysql.js
@@ -0,0 +1,13 @@
+// 引入Mysql模块
+const mysql = require("mysql");
+// 创建数据库连接池
+const pool = mysql.createPool(
+	host: "127.0.0.1",
+	user: "root",
+	password: "hhqweb99",
+	database: "Muisc", // 连接要进入的数据库
+	connectionLimit: 16, // 连接池的数量，默认15
+	multipleStatements: true // 允许同时执行多条SQL命令
+)
+
+module.exports = pool;
