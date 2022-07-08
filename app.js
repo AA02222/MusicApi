161717bbc1e2ab279da5b9ec640381f053bb0e6b
Your Name <you@example.com>
2022-07-08 20:07:58 +0800

    修改了所需要的模块

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 // 引入模块
 const express = require("express");
+// 引入每日推荐路由器
+const RecommRouter = require("./routes/Recommended.js")
 //创建 WEB服务器端
 const app = express();
+//使用中间件
+app.use(express.urlencoded({
+	extended: true
+}))
 //设置端口号
-app.listen(8081, () => {
+let port = 8000;
+app.listen(port, () => {
 	console.log("服务器已经启动了");
-})
+});
+
+
+//为每日推荐添加前缀
+app.use("/recommended", RecommRouter);
